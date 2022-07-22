3f2550f4fad90c587abbf095de93c7f0b9cfc715
Gabriel Machado <gabrielmjj5@gmail.com>
2022-07-22 16:41:33 -0300

    Bug fix

--- frontend/src/components/ClientTable/index.jsx
+++ frontend/src/components/ClientTable/index.jsx
@@ -2,25 +2,29 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { styleTable, styleMainTable } from './styles';
 import axios from 'axios';
+import moment from 'moment';
+// import { Button } from '@mui/material';
 
+const renderData = () => {
+  return (
+    <>
+      <button>1</button>
+      <button>2</button>
+    </>
+
+  )
+}
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'name', headerName: 'Nome', width: 130 },
+  { field: 'id', headerName: 'ID', width: 70},
+  { field: 'name', headerName: 'Nome', width: 130},
   { field: 'city', headerName: 'Cidade', width: 130 },
-  { field: 'uf', headerName: 'UF', width: 90 },
-  {field: 'birthday', headerName: 'Nascimento', width: 130 },
+  { field: 'uf', headerName: 'UF', width: 90},
+  {field: 'birthday', headerName: 'Nascimento', width: 130},
+  {field: 'actions', headerName: '', width:90, renderCell: renderData}
 ];
 
 
-
-// const rows = [
-//   { id: 1, name: 'Gabriel Machado', city: 'Tubarão', uf: 'SC', birthday: '15/03/1999' },
-//   { id: 2, name: 'João', city: 'Tubarão', uf: 'SC', birthday: '15/03/1999' },
-//   { id: 3, name: 'Vitor', city: 'Tubarão', uf: 'SC', birthday: '15/03/1999' },
-//   { id: 4, name: 'Lucas', city: 'Tubarão', uf: 'SC', birthday: '15/03/1999' },
-//   { id: 5, name: 'Diego', city: 'Tubarão', uf: 'SC', birthday: '15/03/1999' },
-// ];
 
 export function ClientTable() {
   const [rows, setRows] = React.useState([]);
@@ -29,12 +33,23 @@ export function ClientTable() {
     axios
       .get('http://localhost:3333/clients')
       .then((response) => {
-        setRows(response.data)
+        // const [{id, name, city, uf, birthday}] = response.data
+        const client = response.data.map((index) => {
+          return {
+            id: index.id,
+            name: index.name,
+            city: index.city,
+            uf: index.uf,
+            birthday: moment(index.birthday).format('DD/MM/YYYY')
+
+          }
+        })
+        setRows(client)
       })
+      //arrumar essa questão criar uma função e chamar sempre que o cadastrar for alterado
   }, [])
-  
-//arrumar essa questão
-  
+  // testesss 
+
   return (
     <div style={styleMainTable}>
       <DataGrid
